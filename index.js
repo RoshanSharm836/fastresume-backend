84033fa453dbec338be2b2f4b74f02d1d48fdaf9
Roshanrocked <roshna837kumar@gmail.com>
2023-03-20 23:13:23 +0530

    backend done

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const router = require("./Routes/resume.model");
 const app = express();
 const PORT = process.env.PORT || 8000;
 const path = require("path");
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 app.use("/", router);
 
 // app.use(express.static(path.join(__dirname, "./resume/build")));
